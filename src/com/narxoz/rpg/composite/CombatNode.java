0f1dac96da789437cb60cb483da6f6fde97df334
Bilal Samat <samat.bilal@narxoz.kz>
2026-03-11 19:27:58 +0500

    Add CombatNode

--- src/com/narxoz/rpg/composite/CombatNode.java
+++ src/com/narxoz/rpg/composite/CombatNode.java
@@ -3,11 +3,11 @@ package com.narxoz.rpg.composite;
 import java.util.List;
 
 public interface CombatNode {
-    String getName();
-    int getHealth();
-    int getAttackPower();
     void takeDamage(int amount);
     boolean isAlive();
     List<CombatNode> getChildren();
     void printTree(String indent);
+    String getName();
+    int getHealth();
+    int getAttackPower();
 }
